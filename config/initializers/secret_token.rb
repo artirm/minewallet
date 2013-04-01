95010426e45fe9c6815f76fcde881b0a8efec4ae
Artirm Pletnev <artirm.pletnev@gmail.com>
2013-04-02 02:25:02 +0400

    Empty Rails project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Minewallet::Application.config.secret_key_base = '4df3340d06210c97cb8a608e049ebb0285b7427c6315cdf340527ae37b2464d1249d991cb4eed193b330294633dc7ed58172fa93bfc16cd382677479a3630f49'
