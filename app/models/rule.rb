8fa454f191b923be4548920521fc84d63357c445
artirm <artirm.pletnev@gmail.com>
2013-04-05 20:39:25 +0400

    heroku prepare

--- app/models/rule.rb
+++ app/models/rule.rb
@@ -1,7 +1,7 @@
 class Rule < Storage
   belongs_to :account
 
-  validates :title, :length => { :minimum => 3, :maximum=>15 }, :uniqueness => true
+  validates :title, :length => { :minimum => 3, :maximum=>35 }, :uniqueness => true
   validates :account_id, :presence => true
   validates_inclusion_of :kind, :in => [0,1]
   validates :tags, :length => { :minimum => 2, :maximum=>25 }, :unless => "tags.blank?"
